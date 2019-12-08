7815cb0e503ca1573a880cff722014527f062dc8
Vagarth Gaurav <vagarth.gaurav@gmail.com>
2019-12-08 21:48:18 +0100

    Setup

--- src/main.js
+++ src/main.js
@@ -3,11 +3,22 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+import axios from 'axios'
+
+const base = axios.create({
+  baseURL: 'http://api.formavisioplus.com/v1'
+})
+
+Vue.prototype.$http = base
+
+
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home pa-6">
+    <h1>Training Center dashboard</h1>
+
+    <v-btn @click="register" color="pink" dark class="mx-2">Register</v-btn>
+    <v-btn @click="signin" color="yellow darken-2" dark>Sign In</v-btn>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  components: {},
+  created() {},
+  methods: {
+    register() {
+      this.$http
+        .post("/register/role/training_center", {
+          firstname: "JET",
+          lastname: "Test",
+          gender: "M",
+          password: "abcd1234",
+          address: "Puranics Rumah Bali",
+          dateOfBirth: "1993-10-13",
+          phoneNumber: "7022815930",
+          email: "vagarth.gaurav@gmail.com"
+        })
+        .then(response => {
+          console.log(response);
+        })
+        .catch(e => {
+          console.error(e);
+        });
+    },
+
+    signin(){
+      
+      this.$http
+        .post("/authenticate/type/training_center", {
+          username: "vagarth.gaurav@gmail.com",
+          password: "abcd1234",
+        })
+        .then(r => {
+          console.log(r);
+        })
+        .catch(e => {
+          console.error(e);
+        });
+
+    }
   }
-}
+};
 </script>
